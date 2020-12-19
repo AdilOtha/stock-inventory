13e33cc02064b79bef52c9830a540f5a689f6097
Adil Otha <adilotha@gmail.com>
2020-12-19 16:08:02 +0530

    Update session.php

--- php_action/session.php
+++ php_action/session.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	require 'db_connect.php';
-	if($_SESSION['login']==NULL){
-		header("location: ../login.php");
+	if(!isset($_SESSION['login'])){
+		header("location: ./login.php");
 	}
 ?>
